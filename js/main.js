dfc40547d334678331f77ca14f6d626c79614b20
Le-Marcelo <marcelotemporini36@gmail.com>
2025-01-29 12:57:17 -0300

    Modal

--- js/main.js
+++ js/main.js
@@ -1,15 +1,23 @@
 import { carregarCSV, processarCSV } from './modules/dataReader.js';
 import { criarBotoesCSV } from './modules/dinamicHtml.js';
 
-// URL do arquivo CSV
-const urlCSV = "/data/data.csv";
+// URL dos arquivos CSV
+const CSVcliente = "/data/cliente.csv";
+const CSVsistema = "/data/sistema.csv";
 
 // Executar as funções
 (async () => {
   try {
-    const conteudoCSV = await carregarCSV(urlCSV);
-    const dados = processarCSV(conteudoCSV);
-    criarBotoesCSV(dados, "botoes-container");
+    //Carregar, processar CSV e criar botões com base nas informações
+    const dadosCliente = processarCSV(await carregarCSV(CSVcliente));
+    const dadosSistema = processarCSV(await carregarCSV(CSVsistema));
+    criarBotoesCSV(dadosCliente, "botoes-cliente");
+
+    //EventListener para o modal fechar
+    document.getElementById("closeModal").addEventListener("click", () => {
+      document.getElementById("modal").style.display = "none";
+    });
+
   } catch (erro) {
     console.error(`Erro: ${erro.message}`);
   }
--- js/modules/dinamicHtml.js
+++ js/modules/dinamicHtml.js
@@ -1,3 +1,6 @@
+//TODO: Separar responsabilidades para apenas criação genérica de botoes com base em um CSV
+//Criar uma outra classe para alterar a função do botão
+
 // Função para criar botões dinamicamente
 export function criarBotoesCSV(dados, containerId) {
   //Checar se o container existe
@@ -8,8 +11,25 @@ export function criarBotoesCSV(dados, containerId) {
 
   dados.forEach((dado) => {
     const botao = document.createElement("button");
-    botao.textContent = dado.nomeCliente;
-    botao.addEventListener("click", () => {});
+    botao.textContent = dado.nome;
+
+    //Caso seja single tenant irá pular para a seleção de ambiente
+    if (dado.multiTenant == "Sim") {
+      botao.setAttribute("data-title", "Selecione o Sistema");
+      botao.setAttribute("data-content", 'Conteúdo do primeiro botão');
+    }else{
+      botao.setAttribute("data-title", "Selecione o Ambiente");
+      botao.setAttribute("data-content", "Conteúdo do primeiro botão");
+    }
+
+    botao.addEventListener("click", () => {
+      const title = botao.getAttribute("data-title");
+      const content = botao.getAttribute("data-content");
+
+      document.getElementById("modalTitle").textContent = title;
+      document.getElementById("modalContent").textContent = content;
+      document.getElementById("modal").style.display = "block";
+    });
     container.appendChild(botao);
   });
 }
